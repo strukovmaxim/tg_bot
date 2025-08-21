8b4c45df0d10f9dc8e0099eb51f6a67ab65b828d
strukovmaxim <42888707+strukovmaxim@users.noreply.github.com>
2025-08-21 19:25:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-API_TOKEN = "7638897879:AAGhxCf1nBPNBmVWiXCKjvqdAJWBsj-Jc0k"
-ADMIN_ID = 136480596
+import os
+
+# Рекомендуется хранить токен в переменных окружения (Railway/GitHub)
+API_TOKEN = os.getenv("API_TOKEN", "7638897879:AAGhxCf1nBPNBmVWiXCKjvqdAJWBsj-Jc0k")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "136480596"))
