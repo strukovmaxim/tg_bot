3d80162781befe9c8f889700cf1d7304a0a3a7d4
strukovmaxim <42888707+strukovmaxim@users.noreply.github.com>
2025-08-21 18:30:35 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,10 +1,10 @@
+from start import register_start_handlers
 from orders import register_order_handlers
 from admin import register_admin_handlers
 from history import register_history_handlers
-from start import register_start_handlers  
 
 def register_handlers(dp):
-    register_start_handlers(dp)
+    register_start_handlers(dp)     # ← важно, первым!
     register_order_handlers(dp)
     register_admin_handlers(dp)
     register_history_handlers(dp)
