381e28d2d051143da575f88a04fd197cf93d3c25
strukovmaxim <42888707+strukovmaxim@users.noreply.github.com>
2025-08-21 18:21:36 +0300

    Create handlers.py

---
+++ handlers.py
@@ -0,0 +1,8 @@
+from orders import register_order_handlers
+from admin import register_admin_handlers
+from history import register_history_handlers
+
+def register_handlers(dp):
+    register_order_handlers(dp)
+    register_admin_handlers(dp)
+    register_history_handlers(dp)
