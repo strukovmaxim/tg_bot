895e54e1276f9a779d260bf80f12443e34a9d238
strukovmaxim <42888707+strukovmaxim@users.noreply.github.com>
2025-08-21 18:35:41 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,10 +1,13 @@
-from start import register_start_handlers
 from orders import register_order_handlers
 from admin import register_admin_handlers
 from history import register_history_handlers
+from start import register_start_handlers
 
 def register_handlers(dp):
+    # сначала бизнес-логика
     register_order_handlers(dp)
     register_admin_handlers(dp)
     register_history_handlers(dp)
-    register_start_handlers(dp)  
+
+    # и только потом fallback (/start и универсальный ответ)
+    register_start_handlers(dp)
